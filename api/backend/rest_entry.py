894a9206d23f9d4f944cc9e53b07159caf6decb7
Emily Wang <148884904+wylome@users.noreply.github.com>
2025-04-14 18:47:22 -0400

    Teams Blueprint + All Routes under teams

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -4,6 +4,8 @@ from backend.db_connection import db
 from backend.customers.customer_routes import customers
 from backend.products.products_routes import products
 from backend.simple.simple_routes import simple_routes
+from backend.teams.teams_routes import teams
+
 import os
 from dotenv import load_dotenv
 
@@ -40,8 +42,7 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(teams, url_prefix='/t')
 
     # Don't forget to return the app object
     return app
