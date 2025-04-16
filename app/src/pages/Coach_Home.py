2ac908c5d269239a5cd8e0023f68d0267968b887
shelbyyisr <shelbyyisr@gmail.com>
2025-04-15 23:30:01 -0400

    Update Coach_Home.py
    Display link and button to Team Stats page. Allow the coach to manage the roster by user inputting a new player with their information as well as removing a player using PlayerID.

--- app/src/pages/Coach_Home.py
+++ app/src/pages/Coach_Home.py
@@ -50,3 +50,28 @@ st.markdown("### Current Plays")
 with st.container():
     for strat in st.session_state.strategies:
         st.markdown(f"- {strat}")
+
+st.markdown("---")
+st.subheader("Team Stats")
+
+# Link to team stats page
+if st.button("View Team Stats", use_container_width=True):
+    st.switch_page("pages/Team_Stats.py")
+
+st.markdown("---")
+st.subheader("Manage Roster")
+
+# Add user input new player
+with st.expander("Player"):
+    st.text_input("First Name")
+    st.text_input("Last Name")
+    st.text_input("Gender")
+    st.text_input("GPA")
+    st.text_input("Grade Level")
+    st.text_input("Height")
+    st.text_input("Position")
+    st.text_input("Recruitment Status")
+
+# Remove player using PlayerID
+with st.expander("Remove Player"):
+    st.text_input("Enter Player ID")
