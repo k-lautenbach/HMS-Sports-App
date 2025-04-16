6b27d13c2634cbabab52a5fbceb17d7527d27c2b
Noah Burra <noahpburra@gmail.com>
2025-04-15 22:25:16 -0400

    changes to athlete page

--- app/src/pages/Athlete_Home.py
+++ app/src/pages/Athlete_Home.py
@@ -39,25 +39,3 @@ with col2:
 st.markdown("---")
 
 st.subheader("Profile")
-
-profile_col1, profile_col2 = st.columns([3, 1])
-
-with profile_col1:
-    st.markdown(f"**Name:** {info['FirstName']} {info['LastName']}")
-    st.markdown(f"**Gender:** {info['Gender']}")
-    st.markdown(f"**GPA:** {info['GPA']}")
-    st.markdown(f"**Grade Level:** {info['GradeLevel']}")
-    st.markdown(f"**Height:** {info['Height']} ft")
-    st.markdown(f"**Position:** {info['Position']}")
-    st.markdown(f"**Recruitment Status:** {info['RecruitmentStatus']}")
-
-with profile_col2:
-    st.image("assets/troyboltonpfp.jpeg", width=500)
-
-st.markdown("---")
-
-# Compare Schools
-st.header("Compare Schools")
-st.markdown("Find your best fit")
-if st.button("Compare Now"):
-    st.switch_page("pages/Compare_Collges.py")
